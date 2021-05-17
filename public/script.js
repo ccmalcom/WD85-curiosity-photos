a5656ec1a88c09ff8c97ced4948281bd7ee235ae
chase malcom <ChaseCMalcom@gmail.com>
2021-05-17 15:37:56 -0400

    finalized page buttons with error handling

--- public/script.js
+++ public/script.js
@@ -17,19 +17,28 @@ today = yyyy + '-' + mm + '-' + dd;
 document.getElementById("inputDate").setAttribute("max", today);
 
 let submitBtn = document.querySelector('#button')
+let nextBtn = document.querySelector('.next')
+let previousBtn = document.querySelector('.prev')
 let submitForm = document.querySelector('form')
 let dateOfPhotos = document.querySelector('#inputDate')
 let cameraSelect = document.querySelector('#cameraSelect');
 let infoDiv = document.querySelector('#info')
 let totalImg = document.querySelector('#totalImg')
 infoDiv.style.display = 'none'
+let nav = document.querySelector('nav')
 let pageNumber = 1;
-submitForm.addEventListener('submit', roverPhotos, totalImage)
+submitForm.addEventListener('submit', totalImage)
+nextBtn.addEventListener('click', nextPage)
+previousBtn.addEventListener('click', prevPage)
+
+nav.style.display = 'none'
 
 let imageCount = 0;
 
 function totalImage(e) {
     e.preventDefault();
+    infoDiv.style.display = 'flex'
+    infoDiv.style.flexDirection = 'column'
     if (cameraSelect.value == 0) {
         url = baseURL + dateOfPhotos.value + '&api_key=' + apiKey
         console.log('URL: ', url);
@@ -39,6 +48,8 @@ function totalImage(e) {
             })
             .then(function (json) {
                 imageCount = json.photos.length 
+                totalImg.innerText = `Total IMG: ${imageCount} (25 per page) `
+                roverPhotos(e);
             })
         } else {
             url = baseURL + dateOfPhotos.value + '&camera=' + cameraSelect.value  +'&api_key=' + apiKey
@@ -49,9 +60,11 @@ function totalImage(e) {
             })
             .then(function (json) {
                 imageCount = json.photos.length
+                totalImg.innerText = `Total IMG: ${imageCount} (25 per page)`
+                roverPhotos(e);
             })
+        }
     }
-}
 function roverPhotos(e) {
     e.preventDefault();
     if (cameraSelect.value == 0) {
@@ -61,9 +74,9 @@ function roverPhotos(e) {
             .then(function (result) {
                 return result.json();
             })
-            .then(function (json) {
-                console.log(json);
-                displayPhotos(json);
+            .then(function (rImg) {
+                console.log(rImg);
+                displayPhotos(rImg);
             })
     } else {
         url = baseURL + dateOfPhotos.value + '&camera=' + cameraSelect.value  + '&page=' + pageNumber +'&api_key=' + apiKey
@@ -72,24 +85,22 @@ function roverPhotos(e) {
             .then(function (result) {
                 return result.json();
             })
-            .then(function (json) {
-                console.log(json);
-                displayPhotos(json);
+            .then(function (rImg) {
+                console.log(rImg);
+                displayPhotos(rImg);
             })
     }
 }
 let imageDiv = document.querySelector('#imgHere')
-function displayPhotos(json) {
+function displayPhotos(rImg) {
     while (imageDiv.firstChild) {
         imageDiv.removeChild(imageDiv.firstChild)
     }
-    infoDiv.style.display = 'flex'
-    infoDiv.style.flexDirection = 'column'
-    if (json.photos.length === 0) {
-        alert('There are no photos for the selected date')
+    if (rImg.photos.length === 0) {
+        if(!alert('There are no photos for the selected date')){ window.location.reload(); }
     } else {
-        for (i = 0; i < json.photos.length; i++) {
-            let photoURL = json.photos[i].img_src;
+        for (i = 0; i < rImg.photos.length; i++) {
+            let photoURL = rImg.photos[i].img_src;
             console.log(photoURL);
             let cdiv = document.createElement('div')
             cdiv.setAttribute('class', 'col')
@@ -110,7 +121,7 @@ function displayPhotos(json) {
 
             let idNum = document.createElement('p')
             idNum.setAttribute('class', 'card-text')
-            idNum.innerHTML = 'Photo ID: ' + json.photos[i].id + '<br>' + 'Camera: ' + json.photos[i].camera.name
+            idNum.innerHTML = 'Photo ID: ' + rImg.photos[i].id + '<br>' + 'Camera: ' + rImg.photos[i].camera.name
 
             imageDiv.appendChild(cdiv)
             cdiv.appendChild(heightDiv)
@@ -120,9 +131,50 @@ function displayPhotos(json) {
             cardBody.appendChild(idNum)
             console.log(imageDiv);
         }
-        
-    }
+    }    
+    nav.style.display = 'flex'
+    nav.style.justifyContent = 'space-between'
+    nav.style.width = '90vw'
+    nav.style.margin = '0 auto 0 auto'
+    nav.style.padding = '0 12px'
+    document.getElementById('displayPage').innerText = pageNumber
+    document.getElementById('displayPage').style.color = 'antiqueWhite'
+}
 
+// if (pageNumber <= 1) { // tests if pageNumber is 0
+//     previousBtn.style.display = "none"; //if pageNumber is 0, hide the prevBtn
+// } else {
+//     previousBtn.style.display = 'block'
+// }
+
+//     if (rImg.length >= 25) { //tests if there are 10 or less articles
+//       nav.style.display = "block";
+//     } else {
+//       nav.style.display = "none"; //hide nav if there are 10 or less results
+//     }  
+
+// if (pageNumber > 1){
+//     previousBtn.style.display = 'block'
+// } else {
+//     previousBtn.style.display = 'none'
+// }
+
+
+
+function nextPage(e){
+    pageNumber++;
+    roverPhotos(e);
+    console.log("Page number:", pageNumber);
+}
+
+function prevPage(e){
+    if(pageNumber >= 1){
+        pageNumber--;
+    } else{
+        return;
+    }
+    roverPhotos(e);
+    console.log("Page:", pageNumber);
 }
 
 // let i=0;
@@ -133,4 +185,3 @@ function displayPhotos(json) {
 //     if (i < txt.length){document.getElementById(thisText).innerHTML += txt.charAt(i); i++;
 //     setTimeout(typeWriter, speed);
 //     }
-// }
